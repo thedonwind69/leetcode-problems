2f3c4de46ba9960ab8544c566d35fe8efbd8692e
Don Nguyen <thedonwind69@gmail.com>
2018-08-27 19:45:04 -0700

    add

---
+++ backspaceStringCompare.js
@@ -0,0 +1,48 @@
+// Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+// Example 1:
+
+// Input: S = "ab#c", T = "ad#c"
+// Output: true
+// Explanation: Both S and T become "ac".
+// Example 2:
+
+// Input: S = "ab##", T = "c#d#"
+// Output: true
+// Explanation: Both S and T become "".
+// Example 3:
+
+// Input: S = "a##c", T = "#a#c"
+// Output: true
+// Explanation: Both S and T become "c".
+// Example 4:
+
+// Input: S = "a#c", T = "b"
+// Output: false
+// Explanation: S becomes "c" while T becomes "b".
+// Note:
+
+// 1 <= S.length <= 200
+// 1 <= T.length <= 200
+// S and T only contain lowercase letters and '#' characters.
+// Follow up:
+
+// Can you solve it in O(N) time and O(1) space?
+
+var redoString = (string) => {
+    var stringArray = string.split("");
+    var array = [];
+    for (let i=0;i<stringArray.length;i++) {
+        if (stringArray[i] !== "#") {
+            array.push(stringArray[i]);
+        } else if (stringArray[i] === "#") {
+            array.pop();
+        }
+    }
+    
+    return array.join("");
+};
+
+var backspaceCompare = function(S, T) {
+    return redoString(S) === redoString(T);
+};
