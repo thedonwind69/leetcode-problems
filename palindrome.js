4b75057ec07313eef9740dda63fd2bae95e00537
Don Nguyen <thedonwind69@gmail.com>
2018-09-25 17:47:05 -0700

    add

---
+++ palindrome.js
@@ -0,0 +1,21 @@
+const is_palindrome = (string) => {
+
+    var stringArray = string.split("");
+    var startIndex = 0;
+    var endIndex = stringArray.length - 1;
+
+    while (startIndex < endIndex) {
+
+        if (stringArray[startIndex] !== stringArray[endIndex]) {
+            return false;
+        } else {
+            startIndex++;
+            endIndex--;
+        }
+
+    }
+
+    return true;
+};
+
+console.log(is_palindrome("fuckkcuf"));
