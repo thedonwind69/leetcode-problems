275174974cd7b9c648596d2bb98dcb7145ceed39
Don Nguyen <thedonwind69@gmail.com>
2019-03-13 19:12:26 -0700

    factorial

--- factorial.js
+++ factorial.js
@@ -1,16 +1,11 @@
 function factorialize(num) {
-
     if (num === 0) {
       return 1;
     }
-  
     let factorial = 1;
-    
     for (let i = 1; i<=num; i++) {
       factorial *= i
     }
-  
     return factorial;
   }
-  
-factorialize(5);
+console.log(factorialize(1));
