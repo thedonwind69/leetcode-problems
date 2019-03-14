ec96da6cf738a19225b6f29c1b8863ac2f8fe896
Don Nguyen <thedonwind69@gmail.com>
2019-03-13 19:11:31 -0700

    add factorial

---
+++ factorial.js
@@ -0,0 +1,16 @@
+function factorialize(num) {
+
+    if (num === 0) {
+      return 1;
+    }
+  
+    let factorial = 1;
+    
+    for (let i = 1; i<=num; i++) {
+      factorial *= i
+    }
+  
+    return factorial;
+  }
+  
+factorialize(5);
