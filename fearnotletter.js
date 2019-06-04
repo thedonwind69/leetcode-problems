798906bf164a3f9c78319d2480727a93c956237d
Don Nguyen <thedonwind69@gmail.com>
2019-06-03 18:44:07 -0700

    add

--- fearnotletter.js
+++ fearnotletter.js
@@ -1,3 +1,34 @@
 
+function fearNotLetter(str) {
+    var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    let startingPoint;
+    for (let i = 0; i < alphabet.length; i++) {
+      if (str[0] === alphabet[i]) {
+        startingPoint = i;
+        break;
+      }
+    }
+    let incremement = 0;
+    var strSplit = str.split("");
+    for (let j = startingPoint; j<alphabet.length; j++) {
+      if (strSplit[incremement] !== alphabet[j]) {
+        return alphabet[j];
+      } else {
+        incremement++;
+      }
+    }
+    return undefined;
+  }
+  
+  fearNotLetter("abce");
 
+//   fearNotLetter("abce") should return "d".
+// Passed
+// fearNotLetter("abcdefghjklmno") should return "i".
+// Passed
+// fearNotLetter("stvwx") should return "u".
+// Passed
+// fearNotLetter("bcdf") should return "e".
+// Passed
+// fearNotLetter("abcdefghijklmnopqrstuvwxyz") should return undefined.
 
