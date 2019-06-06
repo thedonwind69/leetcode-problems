d8a3a9cba498034c16d83437a86d854469d6d257
Don Nguyen <thedonwind69@gmail.com>
2019-06-05 19:32:42 -0700

    add

---
+++ smallestcommonmultiple.js
@@ -0,0 +1,46 @@
+
+// Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+
+// The range will be an array of two numbers that will not necessarily be in numerical order.
+
+// For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
+
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+// Passed
+// smallestCommons([1, 5]) should return a number.
+// Passed
+// smallestCommons([1, 5]) should return 60.
+// Passed
+// smallestCommons([5, 1]) should return 60.
+// Passed
+// smallestCommons([2, 10]) should return 2520.
+// Passed
+// smallestCommons([1, 13]) should return 360360.
+// Passed
+// smallestCommons([23, 18]) should return 6056820.
+
+function smallestCommons(arr) {
+    var array = arr.sort(function(a, b) {
+      return b - a;
+    });
+    var endOfArray = array[array.length-1];
+    var startOfArray = array[0];
+  
+    var multiplier = 1;
+    var biggestNumber = startOfArray;
+    var SCM = 0;
+  
+    for (let i = startOfArray; i >= endOfArray; i--) {
+      if ((biggestNumber * multiplier) % i !== 0 ) {
+        multiplier += 1;
+        i = startOfArray;
+      } else {
+        SCM = biggestNumber * multiplier;
+      }
+    }  
+    return SCM;
+  }
+  
+var answer = smallestCommons([1, 5]);
+console.log(answer);
