c3acddba1b46de84a4d87ac89cc28d9e59eebe9d
Don Nguyen <thedonwind69@gmail.com>
2019-08-21 17:42:28 -0500

    add python practice

---
+++ sex.py
@@ -0,0 +1,8 @@
+def SutheekanSoSexy (string):
+    number = 1
+    while number < 100:
+        if number % 5 == 0:
+            print("Sutheekan is so " + string + "! X " + str(number))
+        number += 1
+
+SutheekanSoSexy("cute")
