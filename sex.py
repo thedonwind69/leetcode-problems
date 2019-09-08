81615efa9e74b67ea0685410a386811ecf0df13d
Don Nguyen <thedonwind69@gmail.com>
2019-09-07 23:02:59 -0500

    game

--- sex.py
+++ sex.py
@@ -1,6 +1,20 @@
-name = format(5, '.2f')
-age = "25"
-print("Hi, I am {} and I am {} years old").format(name, age)
+import random
+
+def heads_or_tails (call, coin, wager, money):
+    if wager <= money:
+        if coin == call:
+            money += wager
+            return ("You won, you have {} dollars!").format(money)
+        else:
+            money -= wager
+            return ("You lost, you have {} dollars left.").format(money)
+    else:
+        return "You dont have that much money!"
+   
+testCoin = ["Heads", "Tails"]
+coinFlip = random.choice(testCoin)
+game1 = heads_or_tails("Heads", coinFlip, 101, 100)
+print(game1)
 
 
-
+        
