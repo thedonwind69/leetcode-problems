cd53efbc617007db8097b3b6744f26ad30dcc007
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-05-27 10:53:38 -0700

    tobeornottobe

---
+++ tobeornottobe.js
@@ -0,0 +1,20 @@
+// Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+// toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+// notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+
+
+var expect = function(val) {
+    function throwError(message) {
+        throw new Error(message);
+    }
+    var obj = {
+        toBe: (val2) => {
+            if (val === val2) {return true} else {throwError("Not Equal")}
+        },
+        notToBe: (val2) => {
+            if (val !== val2) {return true} else {throwError("Equal") }
+        }
+    }
+    return obj
+};
