a340a53973f22c4ca160e39a3b3d26fc7637b0b5
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-06-08 15:09:26 -0700

    countdigitsthatdivideanumber

---
+++ countdigitsthatdivideanumber.js
@@ -0,0 +1,18 @@
+// Given an integer num, return the number of digits in num that divide num.
+
+// An integer val divides nums if nums % val == 0.
+
+var countDigits = function(num) {
+  let count = 0;
+  const digits = String(num).split('');
+
+  for (let i = 0; i < digits.length; i++) {
+    const digit = parseInt(digits[i]);
+    
+    if (digit !== 0 && num % digit === 0) {
+      count++;
+    }
+  }
+
+  return count;
+};
