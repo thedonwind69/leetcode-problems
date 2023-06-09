d2bb13de2c4058332e35af60b15be909b9c4fc8d
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-06-09 15:12:27 -0700

    did one medium question

---
+++ findinguseractiveminutes.js
@@ -0,0 +1,56 @@
+// 1817. Finding the Users Active Minutes
+// Medium
+// 702
+// 266
+// Companies
+// You are given the logs for users' actions on LeetCode, and an integer k. The logs are represented by a 2D integer array logs where each logs[i] = [IDi, timei] indicates that the user with IDi performed an action at the minute timei.
+
+// Multiple users can perform actions simultaneously, and a single user can perform multiple actions in the same minute.
+
+// The user active minutes (UAM) for a given user is defined as the number of unique minutes in which the user performed an action on LeetCode. A minute can only be counted once, even if multiple actions occur during it.
+
+// You are to calculate a 1-indexed array answer of size k such that, for each j (1 <= j <= k), answer[j] is the number of users whose UAM equals j.
+
+// Return the array answer as described above.
+
+ 
+
+// Example 1:
+
+// Input: logs = [[0,5],[1,2],[0,2],[0,5],[1,3]], k = 5
+// Output: [0,2,0,0,0]
+// Explanation:
+// The user with ID=0 performed actions at minutes 5, 2, and 5 again. Hence, they have a UAM of 2 (minute 5 is only counted once).
+// The user with ID=1 performed actions at minutes 2 and 3. Hence, they have a UAM of 2.
+// Since both users have a UAM of 2, answer[2] is 2, and the remaining answer[j] values are 0.
+
+var findingUsersActiveMinutes = function(logs, k) {
+    
+    var tracker = {};
+    for (let i in logs) {
+        let currentLog = logs[i];
+        if (!tracker[currentLog[0]]) {
+            tracker[currentLog[0]] = [];
+            if ( !tracker[currentLog[0]].includes(currentLog[1])) {
+                tracker[currentLog[0]].push(currentLog[1])
+            }
+        } else {
+            if ( !tracker[currentLog[0]].includes(currentLog[1])) {
+                tracker[currentLog[0]].push(currentLog[1])
+            }
+        }
+    }
+
+    var answerArray = new Array(k).fill(0);
+
+    for (let key in tracker) {
+        
+        answerArray[tracker[key].length-1]++
+    }
+
+    return answerArray;
+
+};
+
+const result = findingUsersActiveMinutes([[0,5],[1,2],[0,2],[0,5],[1,3]], 5)
+console.log(result);
