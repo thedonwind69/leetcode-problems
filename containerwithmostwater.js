0ec153f837a5b08fdf89a17c8ffcbadd50bdaf4c
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-06-20 16:35:25 -0500

    edit container water problem

--- containerwithmostwater.js
+++ containerwithmostwater.js
@@ -19,6 +19,8 @@
 // Input: height = [1,1]
 // Output: 1
 
+// my solution. passes 55/61 cases
+
 var maxArea = function(height) {
     var maxWater = 0;
     for (let i = 0; i<height.length; i++) {
@@ -46,7 +48,26 @@ var maxArea = function(height) {
     return maxWater;
 };
 
+// good solution
+var maxArea = function(height) {
+    var leftIndex = 0;
+    var rightIndex = height.length - 1;
+    var maxStoredWater = 0;
+
+    while (leftIndex < rightIndex) {
+        const leftHeight = height[leftIndex];
+        const rightHeight = height[rightIndex];
+        const width = rightIndex - leftIndex;
+        const smallerHeight = leftHeight < rightHeight ? leftHeight : rightHeight;
+        const storedWater = width * smallerHeight;
+
+        maxStoredWater = storedWater > maxStoredWater ? storedWater : maxStoredWater;
+        smallerHeight == leftHeight ? leftIndex++ : rightIndex--;
+    }
+
+    return maxStoredWater;
+};
+
 const height = [1,8,6,2,5,4,8,3,7]
 console.log( maxArea(height) );
 
-// passed 55/61 test cases
