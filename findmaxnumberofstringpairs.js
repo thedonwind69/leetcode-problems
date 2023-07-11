8d07eb5fd1431f1a9ad1921c2d99e42ea3da872b
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-07-11 10:30:29 -0500

    easy question

---
+++ findmaxnumberofstringpairs.js
@@ -0,0 +1,44 @@
+// You are given a 0-indexed array words consisting of distinct strings.
+
+// The string words[i] can be paired with the string words[j] if:
+
+// The string words[i] is equal to the reversed string of words[j].
+// 0 <= i < j < words.length.
+// Return the maximum number of pairs that can be formed from the array words.
+
+// Note that each string can belong in at most one pair.
+
+ 
+
+// Example 1:
+
+// Input: words = ["cd","ac","dc","ca","zz"]
+// Output: 2
+// Explanation: In this example, we can form 2 pair of strings in the following way:
+// - We pair the 0th string with the 2nd string, as the reversed string of word[0] is "dc" and is equal to words[2].
+// - We pair the 1st string with the 3rd string, as the reversed string of word[1] is "ca" and is equal to words[3].
+// It can be proven that 2 is the maximum number of pairs that can be formed.
+// Example 2:
+
+// Input: words = ["ab","ba","cc"]
+// Output: 1
+// Explanation: In this example, we can form 1 pair of strings in the following way:
+// - We pair the 0th string with the 1st string, as the reversed string of words[1] is "ab" and is equal to words[0].
+// It can be proven that 1 is the maximum number of pairs that can be formed.
+
+
+var maximumNumberOfStringPairs = function(words) {
+    var pairs = [];
+    for (let i = 0 ; i<words.length; i++) {
+        let currentWord = words[i]
+        for (let j  = i+1 ; j<words.length; j++) {
+            let nextWord = words[j];
+            if (currentWord === nextWord.split("").reverse().join("")) {
+                pairs.push([currentWord, nextWord.split("").reverse().join("")])
+            }
+        }
+    }
+    return pairs.length;
+};
+
+// easy question
