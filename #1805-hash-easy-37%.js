f623e902c962f4844954ccc91645fe3e95936b6d
Don Anh Nguyen <thedonwind69@gmail.com>
2024-03-19 21:52:59 -0700

    js 1805

---
+++ #1805-hash-easy-37%.js
@@ -0,0 +1,56 @@
+// You are given a string word that consists of digits and lowercase English letters.
+
+// You will replace every non-digit character with a space. For example, "a123bc34d8ef34" will become " 123  34 8  34". Notice that you are left with some integers that are separated by at least one space: "123", "34", "8", and "34".
+
+// Return the number of different integers after performing the replacement operations on word.
+
+// Two integers are considered different if their decimal representations without any leading zeros are different.
+
+ 
+
+// Example 1:
+
+// Input: word = "a123bc34d8ef34"
+// Output: 3
+// Explanation: The three different integers are "123", "34", and "8". Notice that "34" is only counted once.
+// Example 2:
+
+// Input: word = "leet1234code234"
+// Output: 2
+// Example 3:
+
+// Input: word = "a1b01c001"
+// Output: 1
+// Explanation: The three integers "1", "01", and "001" all represent the same integer because
+// the leading zeros are ignored when comparing their decimal values.
+ 
+
+
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var numDifferentIntegers = function(word) {
+    nums = []
+    const numberSet = new Set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    runningStringInt = ""
+    for (let i = 0; i<word.length; i++) {
+        if (numberSet.has(parseInt(word[i]))) {
+            runningStringInt += word[i]
+        } else {
+            if (runningStringInt.length > 0) {
+                nums.push(BigInt(runningStringInt))
+            }
+            runningStringInt = ""
+        }
+    }
+    if (runningStringInt.length > 0) {
+        nums.push(BigInt(runningStringInt))
+    }
+    let count = {}
+    for (let num of nums) {
+        count[num] ? count[num] ++ : count[num] = 1
+    }
+    console.log(count)
+    return Object.keys(count).length
+};
