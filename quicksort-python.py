dbf149f9bbed95fda06b6dcbb0f7bc56b323dede
Don Anh Nguyen <thedonwind69@gmail.com>
2024-06-03 14:37:47 -0700

    quicksort python

---
+++ quicksort-python.py
@@ -0,0 +1,14 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[len(arr) // 2]  # Choose the middle element as the pivot
+        left = [x for x in arr if x < pivot]  # Elements less than the pivot
+        middle = [x for x in arr if x == pivot]  # Elements equal to the pivot
+        right = [x for x in arr if x > pivot]  # Elements greater than the pivot
+        return quick_sort(left) + middle + quick_sort(right)
+
+# Example usage
+arr = [3, 6, 8, 10, 1, 2, 1]
+sorted_arr = quick_sort(arr)
+print(sorted_arr)  # Output: [1, 1, 2, 3, 6, 8, 10]
