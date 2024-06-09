ae1f0bd827445cecfd9598b440f04133d7af459f
Don Anh Nguyen <thedonwind69@gmail.com>
2024-06-09 13:37:12 -0700

    python medium 451

---
+++ 451-hash-medium.py
@@ -0,0 +1,44 @@
+# Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+# Return the sorted string. If there are multiple answers, return any of them.
+
+ 
+
+# Example 1:
+
+# Input: s = "tree"
+# Output: "eert"
+# Explanation: 'e' appears twice while 'r' and 't' both appear once.
+# So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+# Example 2:
+
+# Input: s = "cccaaa"
+# Output: "aaaccc"
+# Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+# Note that "cacaca" is incorrect, as the same characters must be together.
+# Example 3:
+
+# Input: s = "Aabb"
+# Output: "bbAa"
+# Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+# Note that 'A' and 'a' are treated as two different characters.
+ 
+
+class Solution(object):
+    def frequencySort(self, s):
+        count = {}
+        for i, letter in enumerate(s):
+            if letter in count:
+                count[letter] += 1
+            else:
+                count[letter] = 1
+        lettersAndFreq = []
+        for key in count:
+            newObj = [key, count[key]]
+            lettersAndFreq.append(newObj)
+        sorted_data_desc = sorted(lettersAndFreq, key=lambda x: x[1], reverse=True)
+        result = ""
+        for letter in sorted_data_desc:
+            for i in range(letter[1]):
+                result += letter[0]
+        return result 
