2824997ccf0c9479e48bb72412e9e4c292ff37a5
Don Anh Nguyen <thedonwind69@gmail.com>
2024-09-01 20:10:47 -0500

    python 1752

---
+++ 1752-array-easy.py
@@ -0,0 +1,58 @@
+# Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+
+# There may be duplicates in the original array.
+
+# Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
+
+ 
+
+# Example 1:
+
+# Input: nums = [3,4,5,1,2]
+# Output: true
+# Explanation: [1,2,3,4,5] is the original sorted array.
+# You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].
+# Example 2:
+
+# Input: nums = [2,1,3,4]
+# Output: false
+# Explanation: There is no sorted array once rotated that can make nums.
+# Example 3:
+
+# Input: nums = [1,2,3]
+# Output: true
+# Explanation: [1,2,3] is the original sorted array.
+# You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
+ 
+
+ class Solution:
+    def check(self, nums: List[int]) -> bool:
+        i = 0
+        rotationPoints = 0
+        rotationPoint = 0
+        
+        # Identify rotation points
+        while i < len(nums):
+            if i <= len(nums) - 2:
+                if nums[i] > nums[i+1]:
+                    rotationPoints += 1
+                    rotationPoint = i
+            i += 1
+        
+        # If there are no rotation points, the array is already sorted
+        if rotationPoints == 0:
+            return True
+        
+        # If there is more than one rotation point, it cannot be a valid rotation
+        if rotationPoints > 1:
+            return False
+        
+        # If there's exactly one rotation point, check the remainder of the array
+        remainder = nums[rotationPoint+1:] + nums[:rotationPoint+1]
+        
+        # Check if the remainder of the array is in non-decreasing order
+        for i in range(len(remainder) - 1):
+            if remainder[i] > remainder[i + 1]:
+                return False
+        
+        return True
