7d980d9296d3ddfe8d8e5febfa5c98ef568049eb
Don Anh Nguyen <donanhnguyenjobsearch@gmail.com>
2024-11-14 11:47:13 -0800

    392-twopointers-isSubsequence.py

---
+++ 392-twopointers-isSubsequence.py
@@ -0,0 +1,42 @@
+# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+# Example 1:
+
+# Input: s = "abc", t = "ahbgdc"
+# Output: true
+# Example 2:
+
+# Input: s = "axc", t = "ahbgdc"
+# Output: false
+ 
+
+# Constraints:
+
+# 0 <= s.length <= 100
+# 0 <= t.length <= 104
+# s and t consist only of lowercase English letters.
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if s == t:
+            return True
+        sPointer = 0
+        tPointer = 0
+        indexes = []
+        while sPointer < len(s) and tPointer < len(t):
+            currentLetter = s[sPointer]
+            if currentLetter == t[tPointer]:
+                indexes.append(tPointer)
+                sPointer += 1
+                tPointer += 1
+            else:
+                tPointer += 1
+        if len(s) == len(indexes):
+            if indexes == sorted(indexes):
+                return True
+        return False
+        
